39a93c128bb2b5b45734e65e0b98691f24aba86c
Roberto Camargo <robertoc96@outlook.com>
2020-03-15 02:12:24 -0300

    Removed an unnecessary console.log call.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,6 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined);
         } else {
-            console.log(body);
             callback(undefined, {
                 summary,
                 temperature,
